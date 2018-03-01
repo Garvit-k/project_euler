9e161c2a683c907833fd3a3d8c8ebbce50b7569f
Garvit-k <31393258+Garvit-k@users.noreply.github.com>
2018-03-02 01:06:50 +0530

    Added Problem 2

--- src/problem_002/Problem_002.java
+++ src/problem_002/Problem_002.java
@@ -1,24 +1,18 @@
 package problem_002;
 
-import java.math.BigInteger;
-
 public class Problem_002 {
 
 	public static void main(String[] args) {
-		BigInteger sum = BigInteger.ZERO;
-		BigInteger first = BigInteger.ONE;
-		BigInteger second = BigInteger.valueOf(2);
-		BigInteger third = BigInteger.ONE;
-		for(BigInteger i = BigInteger.ONE; i.compareTo(BigInteger.valueOf(4000001)) <= -1; i = i.add(BigInteger.ONE)) {
-			third = first.add(second);
-			//System.out.println("Works");
-			if(third.mod(BigInteger.valueOf(2)).equals(BigInteger.ZERO)) {
-				sum =  sum.add(third);
-			//System.out.println("Works");
-			}
+		int sum = 0;
+		int first = 0;
+		int second = 1;
+		int third = 0;
+		while(third < 4000000) {
+			third = first + second;
+			sum += (third % 2 == 0) ? third : 0;
 			first = second;
 			second = third;
 		}
-		System.out.println(sum.toString()); //1110529252
+		System.out.println(sum); //4613732
 	}
 }
