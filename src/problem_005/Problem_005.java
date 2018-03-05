1397a27d4f9fa9aacc85542e234aeb3a498059e7
Garvit-k <31393258+Garvit-k@users.noreply.github.com>
2018-03-05 15:13:31 +0530

    Added Problem 5

---
+++ src/problem_005/Problem_005.java
@@ -0,0 +1,22 @@
+package problem_005;
+
+public class Problem_005 {
+	public static boolean isDivisible(long number) {
+		boolean flag = true;
+		for(long i = 1; i <= 20; ++i) {
+			if(number % i != 0)
+				flag = false;
+		}
+		return flag;
+	}
+
+	public static void main(String []args) {
+		long i = 1;
+		while(true) {
+			if(isDivisible(i))
+				break;
+			++i;
+		}
+		System.out.println(i);
+	}
+}
